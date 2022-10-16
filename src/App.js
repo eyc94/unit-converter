c8a7236f88cda24f3e4a2fe522d7913b011cd9c5
Eric Chin <ericychin94@gmail.com>
2022-10-16 16:47:44 -0700

    Added info dialog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import UnitConverterBox from "./components/UnitConverterBox";
 import Title from "./components/Title";
+import {Alert} from "@mui/material";
 
 const App = () => {
   return(
     <div>
       <Title />
+      <Alert severity="info">
+        You can now clear your input and result boxes. Try it below!
+      </Alert>
       <UnitConverterBox />
     </div>
   );
